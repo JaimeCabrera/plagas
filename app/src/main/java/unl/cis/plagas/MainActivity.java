68b820c0fab21034181910888d6a7d13c5e6708b
JaimeCabrera <jycabrerap@unl.edu.ec>
2018-10-24 19:39:37 -0500

    Requests permissions
    Request permissions,bottom naviogation.xml, fragments, toolbar.xml

--- app/src/main/java/unl/cis/plagas/MainActivity.java
+++ app/src/main/java/unl/cis/plagas/MainActivity.java
@@ -1,9 +1,9 @@
 package unl.cis.plagas;
 
-import android.Manifest;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
@@ -11,14 +11,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.support.design.widget.Snackbar;
-import android.widget.Button;
-import android.widget.TextView;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 import static android.Manifest.permission.CAMERA;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -35,59 +32,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
        /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
-
-        Button check_permission = (Button) findViewById(R.id.check_permission);
-        Button request_permission = (Button) findViewById(R.id.request_permission);
-        check_permission.setOnClickListener((View.OnClickListener) this);
+        FloatingActionButton request_permission = (FloatingActionButton) findViewById(R.id.request_permission);
         request_permission.setOnClickListener((View.OnClickListener) this);
-
-
     }
-
 
     @Override
     public void onClick(View v) {
-
         view = v;
+        if (!checkPermission()) {
+            requestPermission();
+        } else {
 
-        int id = v.getId();
-        switch (id) {
-            case R.id.check_permission:
-                if (checkPermission()) {
-
-                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
-
-                } else {
-
-                    Snackbar.make(view, "Please request permission.", Snackbar.LENGTH_LONG).show();
-                }
-                break;
-            case R.id.request_permission:
-                if (!checkPermission()) {
-
-                    requestPermission();
-
-                } else {
-
-                    Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
-
-                }
-                break;
+            Snackbar.make(view, "Permission already granted.", Snackbar.LENGTH_LONG).show();
         }
-
     }
 
     private boolean checkPermission() {
         int result = ContextCompat.checkSelfPermission(getApplicationContext(), ACCESS_FINE_LOCATION);
         int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
-
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
 
     private void requestPermission() {
-
         ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, CAMERA}, PERMISSION_REQUEST_CODE);
-
     }
 
     @Override
@@ -95,39 +62,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (requestCode) {
             case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0) {
-
                     boolean locationAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
                     boolean cameraAccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
-
-                    if (locationAccepted && cameraAccepted)
-                        Snackbar.make(view, "Permission Granted, Now you can access location data and camera.", Snackbar.LENGTH_LONG).show();
-                    else {
-
-                        Snackbar.make(view, "Permission Denied, You cannot access location data and camera.", Snackbar.LENGTH_LONG).show();
-
-                        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                            if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
-                                showMessageOKCancel("You need to allow access to both the permissions",
-                                        new DialogInterface.OnClickListener() {
-                                            @Override
-                                            public void onClick(DialogInterface dialog, int which) {
-                                                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                                                    requestPermissions(new String[]{ACCESS_FINE_LOCATION, CAMERA},
-                                                            PERMISSION_REQUEST_CODE);
-                                                }
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermissions(new String[]{ACCESS_FINE_LOCATION, CAMERA},
+                                                        PERMISSION_REQUEST_CODE);
                                             }
-                                        });
-                                return;
-                            }
-                        }*/
-
+                                        }
+                                    });
+                            return;
+                        }
                     }
+
                 }
                 break;
         }
     }
-
-
+   /* ventana para confirma que no desea conceder los permisos */
     private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
         new AlertDialog.Builder(MainActivity.this)
                 .setMessage(message)
@@ -137,3 +94,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 .show();
     }
 }
+
